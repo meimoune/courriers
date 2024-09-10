2c580ca2a483589b0b6ecb27a2c4929821955831
meimoune <maxhmd5050@gmail.com>
2024-09-10 11:58:23 +0000

    Initialisation du projet Spring Boot

---
+++ src/main/java/com/example/courriers/CourriersApplication.java
@@ -0,0 +1,13 @@
+package com.example.courriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourriersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourriersApplication.class, args);
+	}
+
+}
